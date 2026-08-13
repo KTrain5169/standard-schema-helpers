fc0760d9368fd31111621035f8be854eb8393d43
KTrain5369 <me@git.ktrain5369.dev>
2026-08-13 21:45:32 +1000

    chore(tests): add arktype to tests

---
+++ tests/fixtures/arktype.ts
@@ -0,0 +1,6 @@
+import { type } from "arktype";
+
+export const idSchema = type({
+  id: "null",
+  "description?": "string",
+});
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,11 +1,12 @@
 import { expect, test, suite } from "vite-plus/test";
 import * as zSchemas from "./fixtures/zod.ts";
 import * as vSchemas from "./fixtures/valibot.ts";
+import * as aSchemas from "./fixtures/arktype.ts";
 
 import { oneOf } from "../src/index.ts";
 
 suite("oneOf", () => {
-  const idSchema = oneOf([zSchemas.idSchema, vSchemas.idSchema]);
+  const idSchema = oneOf([zSchemas.idSchema, vSchemas.idSchema, aSchemas.idSchema]);
 
   test("use Zod schema", async () => {
     const data = { id: "1" };
@@ -20,6 +21,14 @@ suite("oneOf", () => {
     const testResult = await idSchema["~standard"].validate(data);
     expect(testResult.issues).toBeFalsy();
     const baseResult = await vSchemas.idSchema["~standard"].validate(data);
+    expect(testResult).toStrictEqual(baseResult);
+  });
+
+  test("use ArkType schema", async () => {
+    const data = { id: null };
+    const testResult = await idSchema["~standard"].validate(data);
+    expect(testResult.issues).toBeFalsy();
+    const baseResult = await aSchemas.idSchema["~standard"].validate(data);
     expect(testResult).toStrictEqual(baseResult);
   });
 });
