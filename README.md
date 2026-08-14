52909cbee901285d5547264b4b96cc08e170aafb
KTrain5369 <me@git.ktrain5369.dev>
2026-08-14 15:26:25 +1000

    feat: no point in `union`, add `anyOf` instead

--- README.md
+++ README.md
@@ -11,7 +11,10 @@ Returns a wrapper schema that checks if the input value matches **one** of the p
 ### `allOf`
 
 Returns a wrapper schema that checks if the input value matches **all** of the provided schemas.
+Succesful validation results will be merged together using `Object.assign`.
 
-### `union`
+### `anyOf`
 
-Returns a wrapper schema that discriminates a union by a specific part of the input value.
+Returns a wrapper schema that checks if the input value matches **one or more** of the provided schemas.
+If more than one matches, it will be merged with `Object.assign`, similar to `allOf`.
+This acts as a mix between `allOf` and `oneOf`.
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import type { StandardSchemaV1 } from "@standard-schema/spec";
 
 /**
  * Create a wrapper schema that checks the array of schemas.
- * If the input values matches **one** of the schemas in the array, then it will return with the typed data.
+ * If the input value matches **one** of the schemas in the array, then it will return with the typed data.
  * Otherwise, all schema issues will be compiled and returned as an issue array.
  * @param schemas An array of {@link StandardSchemaV1 Standard Schemas}
  * @returns A {@link StandardSchemaV1 Standard Schema-compliant} wrapper.
@@ -51,7 +51,7 @@ type AllOfOutput<S extends readonly StandardSchemaV1[]> = UnionToIntersection<
 
 /**
  * Creates a wrapper schema that checks the array of schemas.
- * If the input values matches **all** of the schemas in the array, then it will return with the typed data.
+ * If the input value matches **all** of the schemas in the array, then it will return with the typed data.
  * Otherwise, the first schema's issue will be compiled and return as an issue array.
  */
 /* @__NO_SIDE_EFFECTS__ */
@@ -61,8 +61,8 @@ export function allOf<const S extends readonly Pick<StandardSchemaV1, "~standard
   const validate = async (value: unknown) => {
     const results: unknown[] = [];
 
-    for (const schema of schemas) {
-      const result = await schema["~standard"].validate(value);
+    for (const s of schemas) {
+      const result = await s["~standard"].validate(value);
 
       if (result.issues) {
         return result;
@@ -76,6 +76,70 @@ export function allOf<const S extends readonly Pick<StandardSchemaV1, "~standard
     return {
       value: returnObject,
     };
+  };
+
+  return {
+    "~standard": {
+      vendor: "standard-schema-helpers",
+      version: 1,
+      validate,
+    },
+  };
+}
+
+type AnyOfInput<S extends readonly StandardSchemaV1[]> = StandardSchemaV1.InferInput<S[number]>;
+
+type AnyOfOutput<S extends readonly StandardSchemaV1[]> = S extends readonly [
+  infer Head extends StandardSchemaV1,
+  ...infer Tail extends readonly StandardSchemaV1[],
+]
+  ?
+      | StandardSchemaV1.InferOutput<Head>
+      | (Tail extends readonly [] ? never : StandardSchemaV1.InferOutput<Head> & AnyOfOutput<Tail>)
+      | AnyOfOutput<Tail>
+  : never;
+
+/**
+ * Creates a wrapper schema that checks the array of schemas.
+ * If the input value matches **one or more** of the schemas in the array, then it will return with the typed data.
+ * Otherwise, all issues will be compiled together.
+ * This is a mix between `allOf` and `oneOf`.
+ */
+/* @__NO_SIDE_EFFECTS__ */
+export function anyOf<const S extends readonly Pick<StandardSchemaV1, "~standard">[]>(
+  schemas: S,
+): StandardSchemaV1<AnyOfInput<S>, AnyOfOutput<S>> {
+  const validate = async (value: unknown) => {
+    const results: unknown[] = [];
+    const errors: StandardSchemaV1.Issue[] = [];
+
+    for (const s of schemas) {
+      const result = await s["~standard"].validate(value);
+      if (result.issues) {
+        errors.push(...result.issues);
+      } else {
+        results.push(result.value);
+      }
+    }
+
+    if (results.length !== 0) {
+      const finalResult = Object.assign({}, ...results);
+      return {
+        value: finalResult,
+      };
+    } else if (errors.length !== 0) {
+      return {
+        issues: errors,
+      };
+    } else {
+      return {
+        issues: [
+          {
+            message: "No schema matched, but no issues were returned by any validator.",
+          },
+        ],
+      };
+    }
   };
 
   return {
